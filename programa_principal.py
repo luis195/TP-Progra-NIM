a581bef418e8a326b0ba87bcc5ada918ff392dd6
Luis Andrade <luisandrade195@gmail.com>
2022-06-02 12:21:08 -0300

    se agregan nuevas funciones y el programa funciona de manera basica

--- programa_principal.py
+++ programa_principal.py
@@ -1,19 +1,30 @@
 import seleccion_elemento
 import tablero
 
-titulo= "JUEGO DE NIM" 
-titulo1= titulo.center(60,"=")
-print (titulo1)
+titulo = "JUEGO DE NIM"
+titulo1 = titulo.center(60, "=")
+print(titulo1)
 
-print ('Este juego depende de dos factores:')
-print ('1.	El jugador que comienza primero, generalmente gana si hace uso de estrategias.')
-print ('2.	La configuración inicial de las pilas / montones.')
-print ('Necesita dos jugadores.\n Se basa en estrategias matemáticas o estrategias ganadoras.\n El último jugador que extrae las pilas pierde.')
+print('Este juego depende de dos factores:')
+print('1.	El jugador que comienza primero, generalmente gana si hace uso de estrategias.')
+print('2.	La configuración inicial de las pilas / montones.')
+print('Necesita dos jugadores.\n Se basa en estrategias matemáticas o estrategias ganadoras.\n El último jugador que '
+      'extrae las pilas pierde.')
 
 """Aqui unimos todas las funciones."""
 
-juegoTablero = tablero.creartablero(6)
+juegoTablero = tablero.creartablero(2)
 tablero.imprimirtablero(juegoTablero)
+jugador = 1
 
-seleccion_elemento.quitarelemento(juegoTablero)
-tablero.imprimirtablero(juegoTablero)
+while not (seleccion_elemento.hay_ganador(juegoTablero)):
+
+    print("Jugador " + str(jugador) + " es su turno")
+
+    seleccion_elemento.quitarelemento(juegoTablero)
+    tablero.imprimirtablero(juegoTablero)
+
+    if seleccion_elemento.hay_ganador(juegoTablero):
+        print("El Jugador " + str(jugador) + " ha ganado")
+    else:
+        jugador = 2
--- seleccion_elemento.py
+++ seleccion_elemento.py
@@ -1,18 +1,18 @@
-def leerNumeros (mensaje):
+def leerNumeros(mensaje):
     while True:
         try:
-            nro= int (input (mensaje))
+            nro = int(input(mensaje))
             break
         except ValueError:
-            print ('Dato invalido')
-            print ('Intentelo de nuevo')
+            print('Dato invalido')
+            print('Intentelo de nuevo')
 
     return nro
 
-def quitarelemento(tablero):
 
-    filaSeleccion = leerNumeros ("Introdzca el número de la fila de donde desea eliminar elementos: \n")
-    numeroElementosEliminar = leerNumeros ("introduzca cuantos elementos desea eliminar de la fila: \n")
+def quitarelemento(tablero):
+    filaSeleccion = leerNumeros("Introdzca el número de la fila de donde desea eliminar elementos: \n")
+    numeroElementosEliminar = leerNumeros("introduzca cuantos elementos desea eliminar de la fila: \n")
 
     tablero[filaSeleccion - 1].count('|') - 1
 
@@ -21,4 +21,19 @@ def quitarelemento(tablero):
         numeroElementosEliminar = numeroElementosEliminar - 1
     return tablero
 
+
+def hay_ganador(tablero):
+    fila = 0
+    hayGanador = False
+    cantidadElementos = 0
+    while fila < len(tablero):
+        columna = 0
+        while columna <= fila:
+            if tablero[fila][columna] == '|':
+                cantidadElementos = cantidadElementos + 1
+            columna = columna + 1
+        fila = fila + 1
+    if cantidadElementos == 1:
+        hayGanador = True
+    return hayGanador
 
