6216e6785d5edf668d62a8dc3ecf155c9ef5450f
MaruCadi <marielacadima@gmail.com>
2022-06-05 15:09:54 -0300

    titulo

--- programa_principal.py
+++ programa_principal.py
@@ -1,18 +1,23 @@
 import seleccion_elemento
 import tablero
 
-
+print ('')
 titulo = "JUEGO DE NIM"
-titulo1 = titulo.center(60, "=")
+titulo1 = titulo.center(70, "=")
 print(titulo1)
+print ('')
+print ('')
 
 print('Este juego depende de dos factores:')
-print('1.	El jugador que comienza primero, generalmente gana si hace uso de estrategias.')
-print('2.	La configuración inicial de las pilas / montones.')
-print('Necesita dos jugadores.\n Se basa en estrategias matemáticas o estrategias ganadoras.\n El último jugador que '
-      'extrae las pilas pierde.')
+print ('')
+print('1.	El jugador que comienza primero generalmente es el ganador, solo si utiliza estrategias.')
+print('2.	Necesita dos jugadores.')
+print ('')
+print('Ademas se basa en estrategias matemáticas.\n El último jugador que extrae las pilas pierde. \n')
 
-"""Aqui unimos todas las funciones."""
+print ('')
+print ('')
+
 
 def leerNumeros (mensaje):
     while True:
